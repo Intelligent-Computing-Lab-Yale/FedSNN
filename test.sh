80b11634fc656e0e344cce11b38b904f944ce8e6
Yeshwanth V <yeshwanthv5@gmail.com>
2021-03-04 14:12:51 -0500

    Add scripts for fast experimentation

---
+++ test.sh
@@ -0,0 +1,1 @@
+python main_fed.py --dataset N-MNIST --num_classes 10 --model VGG9 --optimizer SGD --bs 32 --local_bs 32 --lr 0.5 --lr_reduce 5 --epochs 100 --local_ep 2 --eval_every 1 --num_users 1 --frac 1 --gpu 0 --timesteps 20 --snn --straggler_prob 0.0 --grad_noise_stdev 0.0 --result_dir test --iid --num_channels 2
